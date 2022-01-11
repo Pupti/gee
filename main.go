733cfaf5ecd42b8b07be060bee9e77a279220c6f
ADiiana <1790407682@qq.com>
2022-01-12 00:12:42 +0800

    gin框架雏形

--- main.go
+++ main.go
@@ -10,32 +10,25 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"gee"
 	"net/http"
 )
 
-type Engine struct{}
-
-// http.ResponseWriter 是一个接口， 因为接口有其本身的类型，在使用中会带来陷阱
-// http.Request 是指针类型，这样可以节省内存
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/":
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-	case "/hello":
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Head[%q] = %q\n", k, v)
-		}
-	default:
-		fmt.Fprintf(w, "404 NOT FOUND\n", req.URL)
-	}
-}
-
 func main() {
-	engine := new(Engine)
+	r := gee.New()
+
+	r.Get("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.path = %q", req.URL.Path)
+	})
+
+	r.Get("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q", k, v)
+		}
+	})
 
 	// http.listenAndServe 启动服务器
 	// 该函数的第二个参数是一个接口，如果这个接口实现serveHTTP函数，
 	// 只要传入实现serveHTTP接口的实例，所有的http请求就由该实例处理
-	log.Fatal(http.ListenAndServe(":9999", engine))
+	r.Run(":9999")
 }
